4150897cda6391c6f9a989719525b1d11c8c04ca
Rethabile Mokoena <fr.mokoena@gmail.com>
2014-06-02 10:06:06 +0200

    main

--- pacmanduelbot/Program.cs
+++ pacmanduelbot/Program.cs
@@ -11,27 +11,16 @@ namespace pacmanduelbot
         public const String _OUTPUT_FILE_NAME = "game.state";
         static void Main(string[] args)
         {
-            string _filepath1 = @"..\..\..\game.state";
-            var _maze = Maze.Read(_filepath1);
-         
-            
-            //var _maze = Maze.Read(args[0]);
+            //string _filepath1 = @"..\..\..\game.state";
+            //var _maze = Maze.Read(_filepath1); 
 
-            //if (isTheGameStart(_maze))
-              //  PoisonInventory.FillUpPoisonInventory();
-            
-            Bot _Bot = new Bot(_maze);
-
-            _maze = _Bot.MakeMove();
-
-           Maze.Write(_maze, _OUTPUT_FILE_NAME);
              
         }
 
         public static bool isTheGameStart(char[][] _maze)
         {
             var _PILL_COUNT = 0;
-            var result = false;
+            var _result = false;
             for (var x = 0; x < Guide._HEIGHT; x++)
             {
                 for (var y = 0; y < Guide._WIDTH; y++)
@@ -43,8 +32,8 @@ namespace pacmanduelbot
                 }
             }
             if (_PILL_COUNT > Guide._PILLS - 2)
-                result = true;
-            return result;
+                _result = true;
+            return _result;
         }
     }
 }
