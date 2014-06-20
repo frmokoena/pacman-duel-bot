c5966346661201ffb5265832008de122454074f7
Rethabile Mokoena <fr.mokoena@gmail.com>
2014-06-20 11:18:50 +0200

    moved constants to settings, and symbols to Symbols.cs

---
+++ pacmanduelbot/shared/Symbols.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pacmanduelbot.shared
+{
+    class Symbols
+    {
+        public const char _PLAYER_A = 'A';
+        public const char _PLAYER_B = 'B';
+        public const char _WALL = '#';
+        public const char _PILL = '.';
+        public const char _BONUS_PILL = '*';
+        public const char _EMPTY = ' ';
+        public const char _POISON_PILL = '!';
+    }
+}
